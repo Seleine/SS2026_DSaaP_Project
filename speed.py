24489baae49e4377d70ef5eb3eb3b5b798214691
Lepori Selina (leporsel) <leporsel@students.zhaw.ch>
2026-04-27 18:31:58 +0200

    38 write tests and refactor for speedpy (#52)
    * add _ to make functions private (which are used in other functions only)
    
    * add rounding to floats for facilitating testing
    
    * write test for speed.py
    
    * add dev dependencies
    
    * add pr validation
    
    * add testing coverage
    
    * add .coverage file
    
    * add pr validation action

--- speed.py
+++ speed.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 import geopandas as gpd
 
-def calculate_timediff_seconds(later: gpd.GeoSeries, now: gpd.pd.Series) -> float:
+def _calculate_timediff_seconds(later: gpd.GeoSeries, now: gpd.pd.Series) -> float:
     """
     Calculate the difference between two datetime objects in seconds.
 
@@ -23,7 +23,7 @@ def calculate_timediff_seconds(later: gpd.GeoSeries, now: gpd.pd.Series) -> floa
     return timediff.dt.total_seconds()
 
 
-def calculate_distance_by_element(geom_a: gpd.GeoSeries, geom_b: gpd.GeoSeries) -> float:
+def _calculate_distance_by_element(geom_a: gpd.GeoSeries, geom_b: gpd.GeoSeries) -> float:
     """
     Calculate the distance by element.
 
@@ -63,15 +63,15 @@ def calculate_timelag_steplength_speed(data: gpd.GeoDataFrame, datetime_col: str
     if not isinstance(data, gpd.GeoDataFrame):
         raise TypeError("Argument data must be gpd.GeoDataFrame")
     elif not isinstance(datetime_col, str):
-         raise TypeError("Argument datetime_col must be a string")
+        raise TypeError("Argument datetime_col must be a string")
     elif not isinstance(geometry_col, str):
-         raise TypeError("Argument geometry must be a string")
+        raise TypeError("Argument geometry must be a string")
 
     data = data.sort_values(by=datetime_col)
 
-    data["timelag"] = calculate_timediff_seconds(later=data[datetime_col].shift(-1), now=data[datetime_col])
-    data["steplength"] = calculate_distance_by_element(geom_a=data[geometry_col], geom_b=data[geometry_col].shift(-1))
-    data["speed_ms"] = data["steplength"] / data["timelag"]
-    data["speed_kmh"] = data["speed_ms"] * 3.6
+    data["timelag"] = _calculate_timediff_seconds(later=data[datetime_col].shift(-1), now=data[datetime_col])
+    data["steplength"] = round(_calculate_distance_by_element(geom_a=data[geometry_col], geom_b=data[geometry_col].shift(-1)), 3)
+    data["speed_ms"] = round(data["steplength"] / data["timelag"], 3)
+    data["speed_kmh"] = round(data["speed_ms"] * 3.6, 3)
 
     return data
---
+++ tests/test_speed.py
@@ -0,0 +1,53 @@
+import shapely
+import pytest
+import geopandas as gpd
+from datetime import datetime
+import numpy as np
+from geopandas.testing import assert_geodataframe_equal
+from speed import calculate_timelag_steplength_speed
+
+
+def test_calculate_timelag_steplength_speed_should_return_new_geodataframe_when_input_valid():
+    # Arrange
+    sample_geom = gpd.GeoSeries([shapely.Point(2684961.287, 1246073.33), shapely.Point(2684806.122, 1246003.058),
+                                 shapely.Point(2684828.182, 1245955.671)], crs=2056)
+    sample_data = {
+        "track_seg_point_id": [1, 2, 3],
+        "time": [datetime.fromisoformat("2024-04-01 09:49:55+02:00"), datetime.fromisoformat("2024-04-01 09:59:38+02:00"), datetime.fromisoformat("2024-04-01 10:09:37+02:00")],
+        "Month": ["April", "April", "April"],
+        "dayphase": ["Daytime", "Daytime", "Daytime"],
+        "geometry": sample_geom
+    }
+    sample_layer = gpd.GeoDataFrame(sample_data)
+
+    # Act
+    data = calculate_timelag_steplength_speed(
+        data=sample_layer,
+        datetime_col="time",
+        geometry_col="geometry"
+    )
+
+    # Assert
+    expected = gpd.GeoDataFrame({
+        **sample_data, # unpacks dictionaries into keyword arguments
+        "timelag": [583.0, 599.0, np.nan],
+        "steplength": [170.336, 52.270, np.nan],
+        "speed_ms": [0.292, 0.087, np.nan],
+        "speed_kmh": [1.051, 0.313, np.nan],
+    })
+
+    assert_geodataframe_equal(data, expected, check_like=True)
+
+def test_calculate_timelag_steplength_speed_raises_if_data_is_not_geodataframe():
+    with pytest.raises(TypeError):
+        calculate_timelag_steplength_speed(data="not a geodataframe", datetime_col="time", geometry_col="geometry")
+
+def test_calculate_timelag_steplength_speed_raises_if_datetime_col_is_not_string():
+    gdf = gpd.GeoDataFrame()
+    with pytest.raises(TypeError):
+        calculate_timelag_steplength_speed(data=gdf, datetime_col=123, geometry_col="geometry")
+
+def test_calculate_timelag_steplength_speed_raises_if_geometry_col_is_not_string():
+    gdf = gpd.GeoDataFrame()
+    with pytest.raises(TypeError):
+        calculate_timelag_steplength_speed(data=gdf, datetime_col="time", geometry_col=123)
