a0435c92c642ddf53e1d1deb06bdd9dbe6460eb6
Selina <leporsel@students.zhaw.ch>
2026-05-03 21:02:57 +0200

    add function main to main.py

--- src/main.py
+++ src/main.py
@@ -16,128 +16,133 @@ from kde_plot import plot_kde
 from bar_plot_counts import barplot_counts
 
 
-########################################
-# Determine buffer around home coordinates
-########################################
+def main():
+    ########################################
+    # Determine buffer around home coordinates
+    ########################################
 
-home_buffer = home_coords.home_coords.buffer(config.buffer_around_home)
+    home_buffer = home_coords.home_coords.buffer(config.buffer_around_home)
 
-########################################
-# Read in GPS data
-########################################
+    ########################################
+    # Read in GPS data
+    ########################################
 
-data = read_gps_data(
-    file_path=config.file_path, gpx_layer=config.gpx_layer, time_zone=config.time_zone
-)
+    data = read_gps_data(
+        file_path=config.file_path, gpx_layer=config.gpx_layer, time_zone=config.time_zone
+    )
 
-########################################
-# Phases of the Day
-########################################
+    ########################################
+    # Phases of the Day
+    ########################################
 
-data = calculate_phases_of_the_day(data=data)
+    data = calculate_phases_of_the_day(data=data)
 
-########################################
-# Remove Home Coordinates
-########################################
+    ########################################
+    # Remove Home Coordinates
+    ########################################
 
-data = remove_home_points(data=data, home_buffer=home_buffer)
+    data = remove_home_points(data=data, home_buffer=home_buffer)
 
-del home_buffer
+    del home_buffer
 
-########################################
-# Calculate Time Lag and Check Speed Value
-########################################
+    ########################################
+    # Calculate Time Lag and Check Speed Value
+    ########################################
 
-data = calculate_timelag_steplength_speed(
-    data=data, datetime_col="time", geometry_col="geometry"
-)
+    data = calculate_timelag_steplength_speed(
+        data=data, datetime_col="time", geometry_col="geometry"
+    )
 
-# all rows containing a speed value > 48 km/h will be removed
-data = data[data["speed_kmh"] < 48]
+    # all rows containing a speed value > 48 km/h will be removed
+    data = data[data["speed_kmh"] < 48]
 
-########################################
-# Summary Table
-########################################
+    ########################################
+    # Summary Table
+    ########################################
 
-summary_table(data=data, datetime_col="time")
+    summary_table(data=data, datetime_col="time")
 
-########################################
-# Calculate Time Lag and Check Speed Value 2
-########################################
+    ########################################
+    # Calculate Time Lag and Check Speed Value 2
+    ########################################
 
-# Data points with a time lag < 9 minutes will be removed. This faciliates the calculation of static and non-static phases.
-data = data[data["timelag"] >= 540]
+    # Data points with a time lag < 9 minutes will be removed. This faciliates the calculation of static and non-static phases.
+    data = data[data["timelag"] >= 540]
 
-# generate new unique id
-data["track_seg_point_id"] = range(1, len(data) + 1)
+    # generate new unique id
+    data["track_seg_point_id"] = range(1, len(data) + 1)
 
-# Now the speed value and the time lag has to be recalculated.
-data = calculate_timelag_steplength_speed(
-    data=data, datetime_col="time", geometry_col="geometry"
-)
+    # Now the speed value and the time lag has to be recalculated.
+    data = calculate_timelag_steplength_speed(
+        data=data, datetime_col="time", geometry_col="geometry"
+    )
 
-########################################
-# Summary Table 2
-########################################
+    ########################################
+    # Summary Table 2
+    ########################################
 
-summary_table(data=data, datetime_col="time")
+    summary_table(data=data, datetime_col="time")
 
-########################################
-# KDE Plot Phases of the Day
-########################################
+    ########################################
+    # KDE Plot Phases of the Day
+    ########################################
 
-barplot_counts(
-    data=data,
-    x_variable="dayphase",
-    title="Number of Data Points per Day Phase",
-    plot_name="dayphases",
-)
+    barplot_counts(
+        data=data,
+        x_variable="dayphase",
+        title="Number of Data Points per Day Phase",
+        plot_name="dayphases",
+    )
 
-kde_night = calculate_kde_from_gps_points(
-    data=data[data["dayphase"] == "Nighttime"], variable_name="Nighttime"
-)
-kde_day = calculate_kde_from_gps_points(
-    data=data[data["dayphase"] == "Daytime"], variable_name="Daytime"
-)
-kde_dusk = calculate_kde_from_gps_points(
-    data=data[data["dayphase"] == "Dusk"], variable_name="Dusk"
-)
-kde_dawn = calculate_kde_from_gps_points(
-    data=data[data["dayphase"] == "Dawn"], variable_name="Dawn"
-)
+    kde_night = calculate_kde_from_gps_points(
+        data=data[data["dayphase"] == "Nighttime"], variable_name="Nighttime"
+    )
+    kde_day = calculate_kde_from_gps_points(
+        data=data[data["dayphase"] == "Daytime"], variable_name="Daytime"
+    )
+    kde_dusk = calculate_kde_from_gps_points(
+        data=data[data["dayphase"] == "Dusk"], variable_name="Dusk"
+    )
+    kde_dawn = calculate_kde_from_gps_points(
+        data=data[data["dayphase"] == "Dawn"], variable_name="Dawn"
+    )
 
-kde_dayphases = gpd.pd.concat([kde_night, kde_day, kde_dusk, kde_dawn])
+    kde_dayphases = gpd.pd.concat([kde_night, kde_day, kde_dusk, kde_dawn])
 
-plot_kde(data=kde_dayphases, plot_name="kde_dayphases")
+    plot_kde(data=kde_dayphases, plot_name="kde_dayphases")
 
-########################################
-# Movement Analysis
-########################################
+    ########################################
+    # Movement Analysis
+    ########################################
 
-data = create_static_column(data=data, buffer=config.buffer_intersection)
+    data = create_static_column(data=data, buffer=config.buffer_intersection)
 
-sample_plot_static_not_static(
-    data=data, start_date="2024-04-01 00:00:00", end_date="2024-04-01 18:00:00"
-)
+    sample_plot_static_not_static(
+        data=data, start_date="2024-04-01 00:00:00", end_date="2024-04-01 18:00:00"
+    )
 
-########################################
-# KDE Plot Movement Analysis
-########################################
+    ########################################
+    # KDE Plot Movement Analysis
+    ########################################
 
-barplot_counts(
-    data=data,
-    x_variable="static",
-    title="Number of Data Points per Movement Phase",
-    plot_name="movement",
-)
+    barplot_counts(
+        data=data,
+        x_variable="static",
+        title="Number of Data Points per Movement Phase",
+        plot_name="movement",
+    )
 
-kde_static = calculate_kde_from_gps_points(
-    data=data[data["static"] == "Static"], variable_name="Static"
-)
-kde_not_static = calculate_kde_from_gps_points(
-    data=data[data["static"] == "Not Static"], variable_name="Not Static"
-)
+    kde_static = calculate_kde_from_gps_points(
+        data=data[data["static"] == "Static"], variable_name="Static"
+    )
+    kde_not_static = calculate_kde_from_gps_points(
+        data=data[data["static"] == "Not Static"], variable_name="Not Static"
+    )
 
-kde_movement = gpd.pd.concat([kde_static, kde_not_static])
+    kde_movement = gpd.pd.concat([kde_static, kde_not_static])
 
-plot_kde(data=kde_movement, plot_name="kde_movement")
+    plot_kde(data=kde_movement, plot_name="kde_movement")
+
+
+if __name__ == "__main__":
+    main()
