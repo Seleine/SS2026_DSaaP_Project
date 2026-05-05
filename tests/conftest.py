c9040f3db21bf7bc2e044b62076e05b5ae8cb2e9
thi-mona <thimona.chrusciel@bluewin.ch>
2026-05-05 23:42:18 +0200

    Write factory make_sample_layer & create sample_layer fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 import numpy as np
 import geopandas as gpd
+import datetime
 import shapely
 from shapely.geometry import Point, Polygon
 
@@ -28,6 +29,40 @@ def sample_gdf_with_time(sample_gdf):
         tz="Europe/Zurich",
     )
     return gdf
+
+
+def make_sample_layer(crs: int = 2056) -> gpd.GeoDataFrame:
+    geometry = gpd.GeoSeries(
+        [
+            shapely.Point(2684961.287, 1246073.33),
+            shapely.Point(2684806.122, 1246003.058),
+            shapely.Point(2684828.182, 1245955.671),
+        ],
+        crs=crs,
+    )
+
+    data = {
+        "track_seg_point_id": [1, 2, 3],
+        "time": [
+            datetime.fromisoformat("2024-04-01 09:49:55+02:00"),
+            datetime.fromisoformat("2024-04-01 09:59:38+02:00"),
+            datetime.fromisoformat("2024-04-01 10:09:37+02:00"),
+        ],
+        "Month": ["April", "April", "April"],
+        "dayphase": ["Daytime", "Daytime", "Daytime"],
+        "geometry": geometry,
+        "timelag": [583.0, 599.0, np.nan],
+        "steplength": [170.336, 52.270, np.nan],
+        "speed_ms": [0.292, 0.087, np.nan],
+        "speed_kmh": [1.051, 0.313, np.nan],
+    }
+
+    return gpd.GeoDataFrame(data, crs=crs)
+
+
+@pytest.fixture
+def sample_layer():
+    return make_sample_layer(crs=2056)
 
 
 @pytest.fixture
--- tests/test_static_not_static.py
+++ tests/test_static_not_static.py
@@ -7,33 +7,6 @@ from geopandas.testing import assert_geodataframe_equal
 from src.static_not_static import create_static_column
 
 
-def test_create_static_column_should_return_new_geodataframe_when_input_valid():
-    # Arrange
-    sample_geom = gpd.GeoSeries(
-        [
-            shapely.Point(2684961.287, 1246073.33),
-            shapely.Point(2684806.122, 1246003.058),
-            shapely.Point(2684828.182, 1245955.671),
-        ],
-        crs=2056,
-    )
-    sample_data = {
-        "track_seg_point_id": [1, 2, 3],
-        "time": [
-            datetime.fromisoformat("2024-04-01 09:49:55+02:00"),
-            datetime.fromisoformat("2024-04-01 09:59:38+02:00"),
-            datetime.fromisoformat("2024-04-01 10:09:37+02:00"),
-        ],
-        "Month": ["April", "April", "April"],
-        "dayphase": ["Daytime", "Daytime", "Daytime"],
-        "geometry": sample_geom,
-        "timelag": [583.0, 599.0, np.nan],
-        "steplength": [170.336, 52.270, np.nan],
-        "speed_ms": [0.292, 0.087, np.nan],
-        "speed_kmh": [1.051, 0.313, np.nan],
-    }
-    sample_layer = gpd.GeoDataFrame(sample_data)
-
     # Act
     data = create_static_column(data=sample_layer, buffer=8)
 
