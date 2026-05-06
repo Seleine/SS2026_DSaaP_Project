93bf679137abfd9eae63bcdac6fa7284508d6482
thi-mona <thimona.chrusciel@bluewin.ch>
2026-05-06 19:52:14 +0200

    Use fixture & .copy() to not pollute other tests

--- tests/test_speed.py
+++ tests/test_speed.py
@@ -1,52 +1,27 @@
-import shapely
 import pytest
 import geopandas as gpd
-from datetime import datetime
 import numpy as np
 from geopandas.testing import assert_geodataframe_equal
 from src.speed import calculate_timelag_steplength_speed
 
 
-def test_calculate_timelag_steplength_speed_should_return_new_geodataframe_when_input_valid():
-    # Arrange
-    sample_geom = gpd.GeoSeries(
-        [
-            shapely.Point(2684961.287, 1246073.33),
-            shapely.Point(2684806.122, 1246003.058),
-            shapely.Point(2684828.182, 1245955.671),
-        ],
-        crs=2056,
-    )
-    sample_data = {
-        "track_seg_point_id": [1, 2, 3],
-        "time": [
-            datetime.fromisoformat("2024-04-01 09:49:55+02:00"),
-            datetime.fromisoformat("2024-04-01 09:59:38+02:00"),
-            datetime.fromisoformat("2024-04-01 10:09:37+02:00"),
-        ],
-        "Month": ["April", "April", "April"],
-        "dayphase": ["Daytime", "Daytime", "Daytime"],
-        "geometry": sample_geom,
-    }
-    sample_layer = gpd.GeoDataFrame(sample_data)
-
+def test_calculate_timelag_steplength_speed_should_return_new_geodataframe_when_input_valid(sample_layer):
     # Act
-    data = calculate_timelag_steplength_speed(
-        data=sample_layer, datetime_col="time", geometry_col="geometry"
+    result = calculate_timelag_steplength_speed(
+        data=sample_layer.copy(),   # important: avoid side effects
+        datetime_col="time",
+        geometry_col="geometry",
     )
 
     # Assert
-    expected = gpd.GeoDataFrame(
-        {
-            **sample_data,  # unpacks dictionaries into keyword arguments
-            "timelag": [583.0, 599.0, np.nan],
-            "steplength": [170.336, 52.270, np.nan],
-            "speed_ms": [0.292, 0.087, np.nan],
-            "speed_kmh": [1.051, 0.313, np.nan],
-        }
-    )
+    expected = sample_layer.copy()
 
-    assert_geodataframe_equal(data, expected, check_like=True)
+    expected["timelag"] = [583.0, 599.0, np.nan]
+    expected["steplength"] = [170.336, 52.270, np.nan]
+    expected["speed_ms"] = [0.292, 0.087, np.nan]
+    expected["speed_kmh"] = [1.051, 0.313, np.nan]
+
+    assert_geodataframe_equal(result, expected, check_like=True)
 
 
 def test_calculate_timelag_steplength_speed_raises_if_data_is_not_geodataframe():
