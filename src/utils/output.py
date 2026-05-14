7f780d047477262f28cf816169209bbf32681127
thi-mona <thimona.chrusciel@bluewin.ch>
2026-05-14 20:50:40 +0200

    Change output function to not compute paths relative to cwd

--- src/utils/output.py
+++ src/utils/output.py
@@ -1,8 +1,17 @@
 from pathlib import Path
 import os
 
+# def get_output_dir(subdir: str | None = None) -> Path:
+#     base = Path(os.environ.get("OUTPUT_DIR", "plots"))
+#     path = base / subdir if subdir else base
+#     path.mkdir(parents=True, exist_ok=True)
+#     return path
+
 def get_output_dir(subdir: str | None = None) -> Path:
-    base = Path(os.environ.get("OUTPUT_DIR", "plots"))
+    # Project root = parent of src/
+    project_root = Path(__file__).resolve().parent.parent.parent
+
+    base = Path(os.environ.get("OUTPUT_DIR", project_root / "plots"))
     path = base / subdir if subdir else base
     path.mkdir(parents=True, exist_ok=True)
     return path
